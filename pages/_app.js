3939dcf3fd0dd2d4adc682f8d1c264d43ac703cb
vinh-vinh <danghoangnguyenvinh@gmail.com>
2023-03-29 11:28:03 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { LayOut } from "../components";
+import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <LayOut>
+        <Toaster />
+        <Component {...pageProps} />
+      </LayOut>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ sanity-ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'izibsed4',
+    dataset: 'production'
+  }
+})
